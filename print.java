a0d98412823b2339a66da0243ae8e61747506a08
shujat321 <mohammad.shujat.btech2025@sitpune.edu.in>
2026-07-28 12:35:33 +0530

    1 assingment

---
+++ print.java
@@ -0,0 +1,9 @@
+class print
+{
+        public static void main(String[] args){
+                 System.out.println("messi is the goat");
+                 System.out.println("Legend of barca");
+                 
+
+                 
+}}
