8f2bcd8800881284a29e1dc61cd6872c53e20b00
shujat321 <mohammad.shujat.btech2025@sitpune.edu.in>
2026-09-21 23:35:35 +0530

    Add VotingSystem with age validation
    Implement a voting system that validates age for eligibility.

---
+++ assingment-16/VotingSystem.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String m) {
+        super(m);
+    }
+}
+
+public class VotingSystem {
+
+    static void validate(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age is below 18. Not eligible for voting.");
+        }
+        else {
+            System.out.println("Eligible for voting.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter your age: ");
+            int age = sc.nextInt();
+
+            validate(age);
+        }
+        catch (InvalidAgeException e) {
+            System.out.println("Caught Exception: " + e.getMessage());
+            System.out.println("Exception class: " + e.getClass().getSimpleName());
+        }
+
+        sc.close();
+    }
+}
